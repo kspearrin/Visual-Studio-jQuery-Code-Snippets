31db08589ce3486aa2b3fa0aa89cbb2c8724f6d0
Kyle Spearrin <kyle.spearrin@gmail.com>
2014-02-26 19:04:40 -0500

    Updated anonymous function spacing
    Added version unit test
    
    Updated preview image and manifest version for 1.4

--- Tests/SnippetTests.cs
+++ Tests/SnippetTests.cs
@@ -8,13 +8,17 @@ namespace Tests
     [TestClass]
     public class SnippetTests
     {
+        // --- CONFIGURATION ---
         // update path to local project directory
-        private string path = @"C:\Projects\Visual-Studio-jQuery-Code-Snippets\jQueryCodeSnippets";
+        private string m_path = @"C:\Projects\Visual-Studio-jQuery-Code-Snippets\jQueryCodeSnippets";
+        private string m_helpUrl = "https://github.com/kspearrin/Visual-Studio-jQuery-Code-Snippets";
+        private string m_version = "1.4.0";
+        // --- END CONFIGURATION ---
 
         [TestMethod]
         public void SnippetTitlesAreCorrect()
         {
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var filePaths = snippetFile.Split(new string[] { "\\" }, StringSplitOptions.None);
                 var fileName = filePaths[filePaths.Length - 1];
@@ -25,7 +29,6 @@ namespace Tests
 
                 var titleNode = snippetDoc.GetElementsByTagName("Title");
                 var title = titleNode[0].InnerText;
-
                 Assert.IsTrue(snippetName == title);
             }
         }
@@ -33,7 +36,7 @@ namespace Tests
         [TestMethod]
         public void SnippetShortcutsAreCorrect()
         {
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var filePaths = snippetFile.Split(new string[] { "\\" }, StringSplitOptions.None);
                 var fileName = filePaths[filePaths.Length - 1];
@@ -44,7 +47,6 @@ namespace Tests
 
                 var shortcutNode = snippetDoc.GetElementsByTagName("Shortcut");
                 var shortcut = shortcutNode[0].InnerText;
-
                 Assert.IsTrue(snippetName == shortcut);
             }
         }
@@ -52,36 +54,32 @@ namespace Tests
         [TestMethod]
         public void SnippetsHaveDescriptions()
         {
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var snippetDoc = new XmlDocument();
                 snippetDoc.Load(snippetFile);
 
                 var descriptionNode = snippetDoc.GetElementsByTagName("Description");
-
-                Assert.IsTrue(descriptionNode != null && descriptionNode[0].InnerText != null);
+                Assert.IsTrue(descriptionNode != null);
 
                 var description = descriptionNode[0].InnerText;
-
-                Assert.IsTrue(description != string.Empty && description.Length > 10);
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(description) && description.Length > 10);
             }
         }
 
         [TestMethod]
         public void SnippetsHaveAuthors()
         {
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var snippetDoc = new XmlDocument();
                 snippetDoc.Load(snippetFile);
 
                 var authorNode = snippetDoc.GetElementsByTagName("Author");
-
-                Assert.IsTrue(authorNode != null && authorNode[0].InnerText != null);
+                Assert.IsTrue(authorNode != null);
 
                 var author = authorNode[0].InnerText;
-
-                Assert.IsTrue(author != string.Empty && author.Length > 5);
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(author) && author.Length > 5);
             }
         }
 
@@ -90,29 +88,45 @@ namespace Tests
         {
             var helpUrl = "https://github.com/kspearrin/Visual-Studio-jQuery-Code-Snippets";
 
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var snippetDoc = new XmlDocument();
                 snippetDoc.Load(snippetFile);
 
                 var urlNode = snippetDoc.GetElementsByTagName("HelpUrl");
-
-                Assert.IsTrue(urlNode != null && urlNode[0].InnerText != null);
+                Assert.IsTrue(urlNode != null);
 
                 var url = urlNode[0].InnerText;
-
-                Assert.IsTrue(url != string.Empty && url == helpUrl);
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(url) && url == helpUrl);
             }
         }
 
         [TestMethod]
         public void SnippetsAreProperFormattedXml()
         {
-            foreach (var snippetFile in Directory.EnumerateFiles(path, "*.snippet", SearchOption.AllDirectories))
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
             {
                 var contents = File.ReadAllText(snippetFile);
-
                 Assert.IsTrue(contents.Contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>"));
+            }
+        }
+
+        [TestMethod]
+        public void SnippetsHaveCorrectVersion()
+        {
+            foreach (var snippetFile in Directory.EnumerateFiles(m_path, "*.snippet", SearchOption.AllDirectories))
+            {
+                var snippetDoc = new XmlDocument();
+                snippetDoc.Load(snippetFile);
+
+                var snippetNode = snippetDoc.GetElementsByTagName("CodeSnippet");
+                Assert.IsTrue(snippetNode != null);
+
+                var formatAttr = snippetNode[0].Attributes["Format"];
+                Assert.IsTrue(formatAttr != null);
+
+                var format = formatAttr.InnerText;
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(format) && format == m_version);
             }
         }
     }
